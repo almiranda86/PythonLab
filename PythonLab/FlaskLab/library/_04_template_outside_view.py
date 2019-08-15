9e7e63568e53d18c9379e085964dd83d19378efb
Andre Luis Miranda <almiranda86@gmail.com>
2019-08-15 09:29:25 -0300

    Some Flask exercise

---
+++ PythonLab/FlaskLab/library/_04_template_outside_view.py
@@ -0,0 +1,19 @@
+"""Moving the template code to a separate file.
+
+Mixing Python code with HTML is ugly. Templates usually live in their own
+location. By default, flask will look up for templates in a 'templates'
+directory living in the same path as the application.
+
+"""
+
+from flask import Flask
+from flask import render_template  # !Important
+
+app = Flask(__name__)
+
+template = "index.html"
+
+@app.route('/')
+def hello_world():
+    library_name = "Poe"
+    return render_template(template, library_name=library_name)
